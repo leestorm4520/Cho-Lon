3933bfa708b705a09fc552432b966d382884c96f
DuyLe <minhduy4520@gmail.com>
2022-11-11 12:35:08 -0600

    update

--- backend/gql/schema.py
+++ backend/gql/schema.py
@@ -1,13 +1,15 @@
 from graphene import List, Field, Argument, ID, ObjectType, Schema
-from graphene_django import DjangoConnectionField
+from graphene_django.filter import DjangoFilterConnectionField
+from django.db.models import Q
+import django_filters
 from types import UserType, ProductType
 from .ChoLon.models import UserModel, ProductModel
 
 
-class Query(graphene.ObjectType):
+class Query(ObjectType):
     users = List(UserType)
     product = Field(ProductType, id = Argument(ID, required =True))
-    products = DjangoConnectionField(ProductType)
+    products = DjangoFilterConnectionField(ProductType, filterset_class = ProductFilter())
 
     def resolve_users(self, info):
         return UserModel.objects.all()
@@ -17,6 +19,19 @@ class Query(graphene.ObjectType):
 
     def resolve_notes(root, info, **kwargs):
         return ProductModel.objects.all()
+
+class ProductFilter(django_filters.FilterSet):
+    search = django_filters.CharFilter(method = 'filter-search')
+
+    class Meta:
+        model = ProductModel
+        fields = ()
+
+    def filter_search(self, queryset, name, value):
+        return queryset_filter(
+            Q(pName__icontains == value) | Q(pDesc__icontains = value)
+        )
+
 
 schema = Schema(query = Query)
 
