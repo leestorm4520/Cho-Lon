f83eb0e30ca8c1946f751ada726998354e64f9b9
DuyLe <minhduy4520@gmail.com>
2022-11-17 18:17:42 -0600

    update

--- backend/ChoLon/settings.py
+++ backend/ChoLon/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "graphene_django",
-    "ChoLon"
+    "ChoLon",
+    "corsheaders"
 ]
 
 # GraphQL
@@ -49,6 +50,7 @@ GRAPHENE = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -57,6 +59,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "ChoLon.urls"
 
