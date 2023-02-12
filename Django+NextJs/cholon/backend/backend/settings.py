f9722cbdea5fa1e5ed674f98e00466cba2b9fe4e
DuyLe <minhduy4520@gmail.com>
2023-02-12 15:56:51 -0600

    update

--- Django+NextJs/cholon/backend/backend/settings.py
+++ Django+NextJs/cholon/backend/backend/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'corsheaders',
+    'storages',
+    'django_filters',
+
+    'products.apps.ProductsConfig'
 ]
 
 MIDDLEWARE = [
