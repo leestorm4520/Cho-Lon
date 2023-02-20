49a9bd04127cc99b0aff6bd6adef999802b88cae
DuyLe <minhduy4520@gmail.com>
2023-02-19 20:44:21 -0600

    update

--- Django+NextJs/cholon/backend/products/views.py
+++ Django+NextJs/cholon/backend/products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import ProductSerializer
+from .models import Product
 
 # Create your views here.
+
+@api_view(['GET'])
+def getAllProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
