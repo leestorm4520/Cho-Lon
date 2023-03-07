be4eddc42e84574c22a1c68bd98cd6e76ad68417
DuyLe <minhduy4520@gmail.com>
2023-03-06 22:02:33 -0600

    update

--- Django+NextJs/cholon/backend/products/urls.py
+++ Django+NextJs/cholon/backend/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products', views.getAllProducts, name='products')
+    path('products/', views.getAllProducts, name='products'),
+    path('products/<str:pk>/', views.getProduct, name='product')
 ]
--- Django+NextJs/cholon/backend/products/views.py
+++ Django+NextJs/cholon/backend/products/views.py
@@ -1,9 +1,19 @@
+# Django Import
+from django.core import paginator
 from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+# Rest Framework Import
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+
+# Local Import
 from .serializers import ProductSerializer
 from .models import Product
+
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -11,4 +21,12 @@ from .models import Product
 def getAllProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+
+    serializer = ProductSerializer(product, many=False)
+
     return Response(serializer.data)
