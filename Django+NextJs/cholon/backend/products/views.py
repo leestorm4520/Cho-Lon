6be96d374fc811f8e14218a593b15042832635a6
DuyLe <minhduy4520@gmail.com>
2023-03-08 22:37:41 -0600

    update

--- Django+NextJs/cholon/backend/products/views.py
+++ Django+NextJs/cholon/backend/products/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Rest Framework Import
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
@@ -17,12 +18,24 @@ from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
+# Get all products with query
 @api_view(['GET'])
 def getAllProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    query = request.query_params.get('keyword')
+    if query == None:
+        query = ''
+    products = Product.objects.filter(name__icontains=query).order_by('-_id')
 
+    page = request.query_params.get('page')
+    paginator = Paginator(products, 10)
+
+    if page == None:
+        page = 1
+    page = int(page)
+    serializer = ProductSerializer(products, many=True)
+    return Response({'products': serializer.data, 'page': page, 'pages': paginator.num_pages})
+
+# Get a single product
 @api_view(['GET'])
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
@@ -30,3 +43,102 @@ def getProduct(request, pk):
     serializer = ProductSerializer(product, many=False)
 
     return Response(serializer.data)
+
+# Get top products
+@api_view(['GET'])
+def getTopProducts(request):
+    products = Product.objects.filter(rating__gte=4).order_by('-rating')[0:5]
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+# Create a new Product
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    user = request.user
+    product = Product.objects.create(
+        user = user,
+        name = "Product Name ",
+        description = " ",
+        unitPrice = 0,
+        category = "Sample Category ",
+        countInStock = 0
+    )
+    serializer = ProductSerializer(product, many = False)
+    return Response(serializer.data)
+
+# Update a single product
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+    data = request.data
+    product = Product.objects.get(_id=pk)
+    product.name = data['name']
+    product.description = data['description']
+    product.unitPrice = data['unitPrice']
+    product.category = data['category']
+    product.countInStock = data['countInStock']
+
+    product.save()
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+# Delete a product
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response('Product deleted successfully')
+
+# Upload image for a product
+@api_view(['POST'])
+def uploadImage(request):
+    data = request.data
+    product_id = data['product_id']
+    product = Product.objects.get(_id=product_id)
+    product.image = request.FILES.get('image')
+    product.save()
+    return Response('Image was uploaded')
+
+# Rate a product
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createProductReview(request, pk):
+    user = request.user
+    product = Product.objects.get(_id=pk)
+    data = request.data
+
+    # Review already exists
+    alreadyExists = product.review_set.filter(user=user).exists()
+
+    if alreadyExists:
+        content = {'detail': 'Product already reviewed'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    # No Rating
+    elif data['rating'] == 0:
+        content = {'detail':'Please select a rating'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    # Create review
+    else:
+        review = Review.objects.create(
+            user=user,
+            product=product,
+            name=user.first_name,
+            rating=data['rating'],
+            comment=data['comment']
+        )
+
+        reviews = product.review_set.all()
+        product.numReviews = len(reviews)
+
+        total = 0
+        for i in reviews:
+            total += i.rating
+        product.rating = total/len(reviews)
+        product.save()
+
+        return Response('Review Added')
